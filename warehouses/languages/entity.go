7834ed7b74da5a8db1d04fc2c9ae8d056377c079
thanhnv <playhard24h@gmail.com>
2026-08-12 22:21:52 +0700

    - bổ sung language model

---
+++ warehouses/languages/entity.go
@@ -0,0 +1,19 @@
+package languages
+
+import (
+	"time"
+)
+
+// Customer represents a customer in the system
+type Language struct {
+	ID        string    `bson:"_id" json:"_id"`
+	Delete    int       `bson:"delete" json:"delete"`
+	CreatedAt time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
+	Name      string    `bson:"name" json:"name"`
+	Code      string    `bson:"code" json:"code"`
+	Image     string    `bson:"image" json:"image"`
+	Order     int       `bson:"order" json:"order"`
+	Status    int       `bson:"status" json:"status"`
+	AuthorId  int       `bson:"author_id" json:"author_id"`
+}
---
+++ warehouses/languages/model.go
@@ -0,0 +1,122 @@
+package languages
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	libCache "github.com/nguoihanoi/golang_shared/libs/cache"
+	libDb "github.com/nguoihanoi/golang_shared/libs/database"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	bSon "go.mongodb.org/mongo-driver/v2/bson"
+)
+
+var dbMongo *libDb.DatabaseClass
+var languageCollection *libDb.CollectionClass
+var languageCache *libCache.Cache
+
+func InitModel(inDb *libDb.DatabaseClass, inRedisClient *libCache.Cache) {
+	dbMongo = inDb
+	languageCache = inRedisClient
+	languageCollection = dbMongo.NewCollection("languages")
+	languageCollection.UpdateIndex([]libDb.CreateIndex{
+		{
+			Name: "delete_1",
+			Key:  bSon.D{{Key: "delete", Value: 1}},
+		},
+		{
+			Name: "delete_1_status_-1_order_1",
+			Key:  bSon.D{{Key: "delete", Value: 1}, {Key: "status", Value: -1}, {Key: "order", Value: 1}},
+		},
+	})
+}
+
+func CreateLanguage(insertData Language) (output string) {
+	output = ""
+	insertData.ID = primitive.NewObjectID().Hex()
+	curDate := time.Now()
+	insertData.CreatedAt = curDate
+	insertData.UpdatedAt = curDate
+	result := languageCollection.Create(insertData)
+	if result == true {
+		output = insertData.ID
+	}
+	return output
+}
+
+func UpdateLanguage(inId string, inUpdateData bSon.M) bool {
+	output := languageCollection.UpdateId(inId, inUpdateData)
+	if output == true {
+		languageCache.Del("customer_language:" + inId)
+	}
+	return output
+}
+
+func DeleteLanguage(inId string) bool {
+	output := languageCollection.DeleteId(inId)
+	if output == true {
+		languageCache.Del("customer_language:" + inId)
+	}
+	return output
+}
+
+func GetLanguageById(inId string, isCache bool) (output Language) {
+	if isCache == true {
+		cacheData := languageCache.Get("customer_language:" + inId)
+		if cacheData != nil {
+			output, _ = cacheData.(Language)
+			return output
+		}
+	}
+	err := languageCollection.FindById(inId).Decode(&output)
+	if err != nil {
+		output.ID = ""
+	} else {
+		languageCache.Set("customer_language:"+inId, output, 0)
+	}
+	return output
+}
+
+func GetLanguages() (results []Language) {
+	cursor, err := languageCollection.Find(bSon.M{"delete": 0, "status": 1}, bSon.D{{Key: "delete", Value: 1}, {Key: "status", Value: -1}, {Key: "order", Value: 1}}, 0, 0)
+	if err != nil {
+		if err = cursor.All(context.TODO(), &results); err != nil {
+			panic(err)
+		}
+	}
+	return results
+}
+
+func SearchLanguages(filter bSon.M, inSortOrder bSon.D, inPage int64, inLimit int64) (results []Language, total int64) {
+	var wg sync.WaitGroup
+	wg.Add(2) // 2 tác vụ song song
+
+	var findErr error
+	// Tác vụ 1: Lấy danh sách kết quả
+	go func() {
+		defer wg.Done()
+		cursor, err := languageCollection.Find(filter, inSortOrder, inPage, inLimit)
+		if err != nil {
+			findErr = err
+			return
+		}
+		if err := cursor.All(context.TODO(), &results); err != nil {
+			findErr = err
+			return
+		}
+	}()
+
+	// Tác vụ 2: Đếm tổng số lượng bản ghi
+	go func() {
+		defer wg.Done()
+		total = languageCollection.Count(filter)
+	}()
+
+	// Chờ cả 2 goroutine hoàn thành
+	wg.Wait()
+	if findErr != nil {
+		results = []Language{}
+		total = 0
+	}
+	return results, total
+}
