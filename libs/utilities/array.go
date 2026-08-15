f560596e010d85dce6db007738124731039fb3d6
thanhnv <playhard24h@gmail.com>
2026-08-15 10:03:05 +0700

    - update array lib

--- libs/utilities/array.go
+++ libs/utilities/array.go
@@ -31,3 +31,36 @@ func (a *arrayClass) RemoveDuplicates(elements []string, isEmpty bool) []string 
 
 	return result
 }
+
+func SymmetricDifference[T comparable](a, b []T) []T {
+	seen := make(map[T]int)
+	for _, v := range a {
+		seen[v] |= 1
+	}
+	for _, v := range b {
+		seen[v] |= 2
+	}
+	var result []T
+	for v, mask := range seen {
+		if mask == 1 || mask == 2 {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+func Difference[T comparable](a, b []T) []T {
+	bMap := make(map[T]struct{}, len(b))
+	for _, v := range b {
+		bMap[v] = struct{}{}
+	}
+
+	var result []T
+	for _, v := range a {
+		if _, found := bMap[v]; !found {
+			result = append(result, v)
+		}
+	}
+
+	return result
+}
